44ade1fd14c55e66d61afa8ea97a02588a766906
Helton Silva <helton.silva@melhorenvio.com>
2025-05-05 17:29:12 -0300

    feat: active router style

--- components/CFHeader.vue
+++ components/CFHeader.vue
@@ -1,14 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const router = useRoute();
+
+const activeConfig= computed<string>(()=>{
+  return router.name === 'configuracao' ? 'text-green-500' : ''
+})
+
+const activeNotificacao = computed<string>(()=>{
+  return router.name === 'notificacao' ? 'text-green-500' : ''
+})
+</script>
 
 <template>
   <header class="w-full bg-black flex justify-end items-center py-4">
     <nav>
-      <ul class="flex gap-4 mr-8">
+      <ul class="flex gap-7 mr-8">
         <NuxtLink to="/configuracao">
-          <UIcon name="i-lucide-cog" class="size-5" />
+          <UIcon 
+            name="i-lucide-cog"  
+            :class="[activeConfig , 'size-5']"  
+          />
         </NuxtLink>
+        
         <NuxtLink to="/notificacao">
-          <UIcon name="i-lucide-bell-ring" class="size-5" />
+          <UIcon 
+            name="i-lucide-bell-ring" 
+            :class="[activeNotificacao , 'size-5']" 
+          />
         </NuxtLink>
       </ul>
     </nav>
